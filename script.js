f9f45b11c91084170328287fd7d277592b9be8eb
Anjana Sharma <ansu74484@gmail.com>
2025-06-30 13:24:56 +0545

    update contct page

--- script.js
+++ script.js
@@ -16,3 +16,12 @@
         behavior: "smooth"
       });
     };
+
+
+    // Toggle menu on mobile
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
